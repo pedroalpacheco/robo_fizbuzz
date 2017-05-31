2d18afc457289bbdd7e64f962d7b7a1aab341fab
papacheco <pedro.pacheco.a@gmail.com>
2017-05-31 11:13:59 -0300

    Melhora de testes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,7 +9,6 @@ Regras do fizzbuzz
 3.Se a posição for multiplo de 3 e  5: fizzbuzz.
 4.Se for qualquer outra posição fale o próprio número.
 
-parei video 39:25
 """
 from functools import partial
 
@@ -23,7 +22,7 @@ multiple_of_3 = partial(multiple_of,3)
 def robot(pos):
     say = str(pos)
 
-    if multiple_of_3(pos) and multiple_of_5(pos):
+    if multiple_of_3(pos) or multiple_of_5(pos):
         say = 'fizzbuzz'
 
     elif multiple_of_5(pos):
@@ -34,20 +33,25 @@ def robot(pos):
 
     return say
 
+def assert_true(expr):
+    try:
+        assert expr
+    except AssertionError:
+        print(expr)
 
 if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
+    assert_true(1) == '1'
+    assert_true(2) == '2'
+    assert_true(4) == '4'
 
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
+    assert_true(robot(3) == 'fizz')
+    assert_true(robot(6) == 'fizz')
+    assert_true(robot(9) == 'fizz')
 
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
+    assert_true(robot(5) == 'buzz' )
+    assert_true(robot(10) == 'buzz')
+    assert_true(robot(20) == 'buzz')
 
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
+    assert_true(robot(15) == 'fizzbuzz')
+    assert_true(robot(30) == 'fizzbuzz')
+    assert_true(robot(45) == 'fizzbuzz')
